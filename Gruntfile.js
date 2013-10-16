9e2b32f1fb7bbb565447c903db0af39bd3c387ed
Martin Christov <martin.christov@gmail.com>
2013-10-16 11:46:01 +0300

    Setup the environment with angular seed project, grunt and node.js server

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    recess: {
+      options: {
+        compile: true
+      },
+      compileless: {
+        options:{
+          compress:true
+        },
+        src: ['app/less/app.less'],
+        dest: 'app/app.css'
+      }
+    },
+
+    concat:{
+      compilejs:{
+        src:[
+          "app/js/*"
+        ],
+        dest:"app/js/app.min.js"
+      }
+    },
+
+    watch:{
+      recess:{
+        files:["app/less/*"], tasks:['recess']
+      },
+      concat:{
+        files:["app/js/**/*.js"], tasks:['concat']
+      },
+      html:{
+        files:"app/**/*.html",
+        tasks:[]
+      },
+      options:{spawn:false, livereload:true}
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-recess');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  //grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('dist-css', ['recess']);
+  //grunt.registerTask('default', ['less']);
+
+};
