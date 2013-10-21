5f9bc51b554f39068c65f0be14fc7e3d5280268f
Martin Christov <martin.christov@gmail.com>
2013-10-21 14:42:52 +0300

    search view base. depth service for enter/back control.

--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,21 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('kidamom.services', []).
-  value('version', '0.1');
+  value('version', '0.1').
+
+  service('depth', [function () {
+  	var state = 0, maxState = 3;
+  	return {
+  		more:function(){
+  			state++;
+  			if(maxState<state)state = maxState;
+  		},
+  		less:function(){
+  			state--;
+  			if(state<0)state=0;
+  		},
+  		get:function(){
+  			return state;
+  		}
+  	}
+  }])
